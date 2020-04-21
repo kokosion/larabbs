d15d9d4dd89df9da42347cd3d1e1ec252a74af5c
kokosion <kokosion@gmail.com>
2020-04-21 09:38:21 +0800

    增加前端样式

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSFR Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+  <!--{ Styles }-->
+  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+
+    <div class="container">
+      @include('shared._messages')
+
+      @yield('content')
+    </div>
+
+    @include('layouts._footer')
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+  </div>
+</body>
+
+</html>
